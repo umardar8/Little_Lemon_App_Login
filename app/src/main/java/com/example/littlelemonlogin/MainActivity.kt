93c222f383bdbe0e74d20181823b08387e587c5d
umardar8 <umerfarooqdar.official@gmail.com>
2023-09-10 19:44:40 +0500

    Initial commit

---
+++ app/src/main/java/com/example/littlelemonlogin/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.littlelemonlogin
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.littlelemonlogin.ui.theme.LittleLemonLoginTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LittleLemonLoginTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    LoginScreen()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun LoginScreen(){
+    val context = LocalContext.current
+    var username by remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+    var password by remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(
+                id = R.drawable.littlelemonlogo),
+            contentDescription = "Logo Image",
+            modifier = Modifier.padding(10.dp)
+        )
+        TextField(
+            value = username,
+            onValueChange = {username = it},
+            label = { Text(text = "Username") },
+            modifier = Modifier.padding(10.dp)
+        )
+        TextField(
+            value = password,
+            onValueChange = {password = it},
+            label = { Text(text = "Password") },
+            modifier = Modifier.padding(10.dp)
+        )
+        Button(
+            onClick = { if(username.text == "Darian" && password.text == "littlelemon") {
+                Toast.makeText(context, "Access Granted!", Toast.LENGTH_LONG).show() }
+                else {
+                Toast.makeText(context, "Access Denied!", Toast.LENGTH_LONG).show() }
+            },
+            colors = ButtonDefaults.buttonColors(
+                Color(0xFF495E57)
+            ),
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Text(
+                text = "Login",
+                color = Color(0xFFEDEFEE)
+            )
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview(){
+    LoginScreen()
+}
